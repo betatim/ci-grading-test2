d551986baebf2932e68eacbefd5cc2b5727d3ec3
Tim Head <betatim@gmail.com>
2018-09-28 17:01:16 +0200

    initial commit

---
+++ 01-lecture/q-1aaac66.py
@@ -0,0 +1,13 @@
+
+test = {
+  'name': 'test_valid',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [{'code': r"""
+      >>> assert q2.value, "You need to select at least one reply"
+      """},
+      ]
+    }
+  ]
+}
---
+++ 01-lecture/q-4f30b15.py
@@ -0,0 +1,14 @@
+
+test = {
+  'name': 'test_valid',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [{'code': r"""
+      >>> # this will be a public test of the answer
+      >>> assert q1.value, "You need to select one reply"
+      """},
+      ]
+    }
+  ]
+}

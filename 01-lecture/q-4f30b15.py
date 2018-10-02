128ffe3f0391d6492a132c18420e5ea462015ef0
Tim Head <betatim@gmail.com>
2018-10-02 13:12:34 +0200

    Update q-4f30b15.py

--- 01-lecture/q-4f30b15.py
+++ 01-lecture/q-4f30b15.py
@@ -5,7 +5,7 @@ test = {
   'suites': [
     {
       'cases': [{'code': r"""
-      >>> # this will be a public test of the answer
+      >>> #  this will be a public test of the answer
       >>> assert q1.value, "You need to select one reply"
       """},
       ]
